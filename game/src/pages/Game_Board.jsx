1806220725885fe9f72201e2a2c7540f94e4b449
Haileab <womahaileab1428@gmail.com>
2026-09-19 18:13:03 +0300

    working on game board interface

--- game/src/pages/Game_Board.jsx
+++ game/src/pages/Game_Board.jsx
@@ -1,5 +1,7 @@
 import got from "../assets/got.png"
-
+import Tryion2 from "../assets/Tryion2.png"
+import Little_Finger2 from "../assets/Little_Finger2.png"
+import Varys2 from "../assets/Varys2.png"
 function Game_Board(){
  return (
     <div className="min-h-screen ">
@@ -23,6 +25,55 @@ function Game_Board(){
                 className="aspect-16/10 w-full cursor-crosshair  object-cover   hover:scale-[1.01]   transition-transform
                   duration-300"
                 />
+              </div>
+              <div
+              className="border border-[#28506D] shadow-lg p-6  rounded-lg"
+              >
+                <h2 
+                className="text-lg font-semibold text-[#F5F0DF]">Find these characters</h2>
+                <div
+                className="mt-2 flex flex-col gap-3"
+                >
+                  <div
+                  className="flex gap-4 items-center p-2  border border-[#28506D] rounded-lg"
+                  >
+                    <img
+                    src={Tryion2}
+                    alt="tryion"
+                   className="rounded-lg w-22 "
+                    />
+                    <div>
+                         <p className="text-[#F5F0DF] text-md font-semibold font-serif">Tyrion Lannister</p>
+                          
+                     </div>
+
+                  </div>
+                   <div
+                  className="flex gap-4 rounded-lg items-center p-2  border border-[#28506D]"
+                  >
+                    <img
+                    src={Varys2}
+                    alt="varys"
+                    className="rounded-lg  w-22"
+                    />
+                    <div>
+                        <p className="text-[#F5F0DF] text-md font-semibold font-serif">Lord Varys</p>
+                    </div>
+
+                  </div>
+                  <div
+                  className="flex gap-4 rounded-lg items-center p-2  border border-[#28506D]"
+                  >
+                    <img
+                    src={Little_Finger2 }
+                    alt="littleFinger"
+                    className="rounded-lg  w-22"
+                    />
+                    <div>
+                      <p className="text-[#F5F0DF] text-md font-semibold font-serif">Little Finger</p>
+                    </div>
+                  </div>
+                </div>
               </div>
               
          </div>
