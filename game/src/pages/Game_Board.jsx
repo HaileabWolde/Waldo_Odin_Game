7c876fab9e431683a0ca3edc452bf61176d52e22
Haileab <womahaileab1428@gmail.com>
2026-09-22 13:15:25 +0300

    working on drop down menu

--- game/src/pages/Game_Board.jsx
+++ game/src/pages/Game_Board.jsx
@@ -1,21 +1,30 @@
+import { useState } from "react"
 import got from "../assets/got.png"
 import Tryion2 from "../assets/Tryion2.png"
 import Little_Finger2 from "../assets/Little_Finger2.png"
 import Varys2 from "../assets/Varys2.png"
 function Game_Board(){
-  const handleImageClick = e => {
-      const rect = e.target.getBoundingClientRect()
-    
-    // Get click position relative to image
-    const x = e.pageX - rect.left;
-    const y = e.pageY - rect.top;
+  const [dropdown, setDropDown] = useState(null)
 
-    // Normalize to percentage of width and height
-    // Multiply by 10000 and divide by 100 to keep 2 decimal places
-    const xCoord = Math.floor(x / rect.width * 10000)/100;
-    const yCoord = Math.floor(y / rect.height * 10000)/100;
+  const handleImageClick = e => {
+      const rect = e.currentTarget.getBoundingClientRect()
+    
+    
+    // Mouse position inside the image
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    // Convert position to percentages
+    const xCoord = ((x / rect.width) * 100).toFixed(2);
+    const yCoord = ((y / rect.height) * 100).toFixed(2);
+
+    setDropDown({
+      top: yCoord,
+      left: xCoord,
+    });
 
     console.log(`Clicked at: X=${xCoord}%, Y=${yCoord}%`);
+   
 }
  return (
     <div className="min-h-screen ">
@@ -32,7 +41,7 @@ function Game_Board(){
              <div 
             className="rounded-2xl overflow-hidden  border border-[#28506D]
                 bg-[#0B1F33]
-                shadow-2xl">
+                shadow-2xl relative">
                 <img
                 src={got}
                 onClick={handleImageClick}
@@ -40,6 +49,17 @@ function Game_Board(){
                 className="aspect-16/10 w-full cursor-crosshair  object-cover   hover:scale-[1.01]   transition-transform
                   duration-300"
                 />
+                {
+                  dropdown &&
+                  <div
+                  className="absolute  bg-white"
+                  style={{
+                    top:`${dropdown.top}%`, 
+                    left:`${dropdown.left}%`}}
+                  >
+                    <h1>Bitch</h1>
+                  </div>
+                }
               </div>
               <div
               className="border border-[#28506D] shadow-lg p-6  rounded-lg"
